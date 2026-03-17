ce6c97eda1e16172289ad33d467d4226569d0df5
Asmitayadav543 <57805523+Asmitayadav543@users.noreply.github.com>
2026-03-17 20:41:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Home } from "./components/Home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Fruits from "./components/Fruits/Fruits";
+import Dairy from "./components/Dairy/Dairy";
+import Seefood from "./components/Seefood/Seefood";
+import AllProducts from "./components/AllProducts/AllProducts";
+import Layout from "./components/Layout/Layout";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+
+          path: "/",
+          element: <Home />
+        },
+
+        {
+          path: "/fruits",
+          element: <Fruits />,
+
+        },
+        {
+          path: "/dairy",
+          element: <Dairy />
+        },
+        {
+          path: "/seafood",
+          element: <Seefood />
+        },
+        {
+          path: "/all-products",
+          element: <AllProducts />
+        }
+
+
+
+
+
+
+      ]
+
+
+    },
+
+
+  ])
+  return <RouterProvider router={router} />
+}
+export default App;
